9487e694ca56d7014002eea24450259886a21ebc
juratbek <juratbekxudayberganov9@gmail.com>
2024-11-06 11:59:46 +0500

    Pushed

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -23,16 +23,6 @@ export const navbarItems = [
         id: 5,
         path: "/testimonials",
         name: "Testimonials"
-    },
-    {
-        id: 6,
-        path: "/about",
-        name: "About us"
-    },
-    {
-        id: 7,
-        path: "/contact",
-        name: "Contact"
     }
 ]
 
